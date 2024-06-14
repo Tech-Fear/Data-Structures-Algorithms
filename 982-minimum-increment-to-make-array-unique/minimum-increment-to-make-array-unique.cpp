fb79a9bf3fae08e6f158f7ddbf8fdf4112a49697
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-14 10:07:10 +0530

    Time: 108 ms (86.02%) | Memory: 69.1 MB (50.39%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1])
+            {
+                nums[i]=nums[i]+1;
+                ans++;
+            }
+            else if(nums[i]<nums[i-1]){
+                int temp=nums[i-1]-nums[i]+1;
+                nums[i]=nums[i]+temp;
+                ans+=temp;
+            }
+        }
+        return ans;
+    }
+};
