49bb7883413f8e7db2ca4a181152ea4c671d8c02
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-19 22:44:58 +0530

    Time: 5 ms (54.91%) | Memory: 9.6 MB (63.19%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    bool isValid(vector<string>&board,int &row,int &col,int &n){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q') return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void solve(vector<string>&board,int row,int &n){
+        if(row==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(isValid(board,row,col,n)){
+                board[row][col]='Q';
+                solve(board,row+1,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        solve(board,0,n);
+        return ans;
+    }
+};
