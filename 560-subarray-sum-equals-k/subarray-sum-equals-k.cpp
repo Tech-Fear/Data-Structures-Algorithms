c2e335820ac49502b1fcc733a592f9893075444d
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-20 22:06:18 +0530

    Time: 73 ms (57.23%) | Memory: 54.8 MB (8.58%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_map<long long,int>mp;
         long long preSum=0;
         int count=0;
