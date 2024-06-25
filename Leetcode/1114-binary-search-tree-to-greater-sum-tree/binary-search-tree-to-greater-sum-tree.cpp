8848424ad9d5d87bf2fad06a8b62d0c1346eccb8
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-25 09:39:56 +0530

    Time: 5 ms (15.49%) | Memory: 10.2 MB (12.64%) - LeetSync

--- Leetcode/1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ Leetcode/1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -9,39 +9,57 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int>inord;
+//     void inorderTrav(TreeNode* root){
+//         if(root){
+//             inorderTrav(root->left);
+//             inord.push_back(root->val);
+//             inorderTrav(root->right);
+//         }
+//     }
+//     void updateTree(TreeNode* root,map<int,int>mp){
+//         if(root){
+//             updateTree(root->left,mp);
+//             root->val=mp[root->val];
+//             updateTree(root->right,mp);
+//         }
+//     }
+//     TreeNode* bstToGst(TreeNode* root) {
+//         inorderTrav(root);
+//         vector<int>prefixSum(inord.size());
+//         prefixSum[0]=inord[0];
+//         for(int i=1;i<inord.size();i++){
+//             prefixSum[i]=prefixSum[i-1]+inord[i];
+//         }
+//         map<int,int>mp;
+//         int totalSum=prefixSum.back();
+//         for(int i=0;i<inord.size();i++){
+//             mp[inord[i]]=totalSum+inord[i]-prefixSum[i];
+//         }
+//         updateTree(root,mp);
+//         return root;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int>inord;
-    void inorderTrav(TreeNode* root){
-        if(root){
-            inorderTrav(root->left);
-            inord.push_back(root->val);
-            inorderTrav(root->right);
-        }
-    }
-    void updateTree(TreeNode* root,map<int,int>mp){
-        if(root){
-            updateTree(root->left,mp);
-            root->val=mp[root->val];
-            updateTree(root->right,mp);
-        }
-    }
     TreeNode* bstToGst(TreeNode* root) {
-        inorderTrav(root);
-        vector<int>prefixSum(inord.size());
-        prefixSum[0]=inord[0];
-        for(int i=1;i<inord.size();i++){
-            prefixSum[i]=prefixSum[i-1]+inord[i];
-        }
-        map<int,int>mp;
-        int totalSum=prefixSum.back();
-        for(int i=0;i<inord.size();i++){
-            mp[inord[i]]=totalSum+inord[i]-prefixSum[i];
-        }
-        updateTree(root,mp);
+        int sum=0;
+        sumNode(root,sum);
+
         return root;
     }
+    void sumNode(TreeNode* root,int &sum){
+        if(!root) return ;
+            sumNode(root->right,sum);
+            root->val+=sum;
+            sum=root->val;
+            sumNode(root->left,sum);
+    }
 };
+
 auto init=[](){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
