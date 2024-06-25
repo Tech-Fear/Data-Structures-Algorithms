2c91bc0514355da210b6926b14682eb239f28a89
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-25 22:19:04 +0530

    Time: 151 ms (87.38%) | Memory: 119.2 MB (60.19%) - LeetSync

--- Leetcode/234-palindrome-linked-list/palindrome-linked-list.cpp
+++ Leetcode/234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,64 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-    ListNode* prev = nullptr;
-    ListNode* cur = head;
-    ListNode* next = nullptr;
-    
-    while (cur) {
-        next = cur->next;
-        cur->next = prev;
-        prev = cur;
-        cur = next;
+ListNode* findMiddle(ListNode* head){
+    ListNode* fast=head;
+    ListNode *slow=head;
+    while(fast&& fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
     }
-    
-    return prev;
+    return slow;
 }
-    bool isPalindrome(ListNode* head) {
-    if (!head || !head->next) return true;  // Empty list or single node is a palindrome
-    
-    ListNode* slow = head;
-    ListNode* fast = head;
-    ListNode* slow_prev = nullptr;
-    ListNode* mid = nullptr;
-    
-    // Use fast and slow pointers to find the middle of the list
-    while (fast && fast->next) {
-        fast = fast->next->next;
-        slow_prev = slow;
-        slow = slow->next;
-    }
-    
-    // Determine if the list has odd number of nodes
-    if (fast) {  // Odd number of nodes, slow is at the exact middle
-        mid = slow;
-        slow = slow->next;
-    }
-    
-    // Split the list into two halves
-    slow_prev->next = nullptr;
-    
-    // Reverse the second half of the list
-    ListNode* second_half = reverse(slow);
-    
-    // Compare the first half (head) with the reversed second half (second_half)
-    ListNode* p1 = head;
-    ListNode* p2 = second_half;
-    bool is_palindrome = true;
-    
-    while (p1 && p2) {
-        if (p1->val != p2->val) {
-            is_palindrome = false;
-            break;
-        }
-        p1 = p1->next;
-        p2 = p2->next;
-    }
-    
-    // Restore the list to its original state (optional)
-    slow_prev->next = reverse(second_half);
-    
-    return is_palindrome;
+ListNode* reverse(ListNode* head){
+    if(!head|| !head->next) return head;
+    ListNode* temp=reverse(head->next);
+    head->next->next=head;
+    head->next=nullptr;
+    return temp;
 }
+    bool isPalindrome(ListNode* A) {
+       ListNode *mid=findMiddle(A);
+    mid=reverse(mid);
+    while(mid){
+        if(mid->val!=A->val) return false;
+        A=A->next;
+        mid=mid->next;
+    }
+    return true; 
+    }
 };
+auto init=[](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return '#';
+}();
