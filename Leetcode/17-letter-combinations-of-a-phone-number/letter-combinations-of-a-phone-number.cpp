435cc575875cf4cc4127c99be4a9451d9d6a6860
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-06-29 23:06:50 +0530

    Time: 4 ms (15.70%) | Memory: 8.4 MB (32.31%) - LeetSync

---
+++ Leetcode/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.length()==0) return {};
+        vector<string>result;
+        string cur;
+        unordered_map<char,string>mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
+        generate(digits,0,result,cur,mp);
+        return result;
+    }
+private:
+    void generate(string digits,int ind,vector<string>&res,string &cur,unordered_map<char,string>&mp){
+        if(digits.length()==ind){
+            res.push_back(cur);
+        }
+        char digit=digits[ind];
+        string str=mp[digit];
+        for(char letter:str){
+            cur.push_back(letter);
+            generate(digits,ind+1,res,cur,mp);
+            cur.pop_back();
+        }
+    }
+};
