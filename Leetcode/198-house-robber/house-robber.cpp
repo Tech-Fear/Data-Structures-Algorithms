a66e4d5a8ba2224a04c932b09e002954ce7db712
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-07-11 15:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (10.76%) - LeetSync

--- Leetcode/198-house-robber/house-robber.cpp
+++ Leetcode/198-house-robber/house-robber.cpp
@@ -12,6 +12,14 @@ private:
 public:
     int rob(vector<int>& nums) {
         dp.resize(nums.size(),-1);
-        return solve(nums,0);
+        // return solve(nums,0);
+        if (nums.empty()) return 0;
+        if (nums.size() == 1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
     }
 };
