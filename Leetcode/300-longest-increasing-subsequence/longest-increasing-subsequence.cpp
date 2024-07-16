f0c9f73190088df71e99c85fba2db1e8fee3e8bf
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-07-16 16:11:26 +0530

    Time: 64 ms (71.97%) | Memory: 13.3 MB (43.76%) - LeetSync

--- Leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ Leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,17 @@
-typedef vector<int> v32;
-typedef vector<vector<int>> vv32;
 class Solution {
-private:
-    int solve(int i, v32& arr, int n, int idx, vv32& dp) {
-        if (i == n)
-            return 0;
-
-        if (dp[i][idx + 1] != -1)
-            return dp[i][idx + 1];
-
-        int include = 0;
-        if (idx == -1 || arr[idx] < arr[i]) {
-            include = solve(i + 1, arr, n, i, dp) + 1;
-        }
-
-        int exclude = solve(i + 1, arr, n, idx, dp);
-        return dp[i][idx + 1] = max(include, exclude); 
-    }
-
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vv32 dp(n, v32(n + 1, -1));
-        return solve(0, nums, n, -1, dp);
+        int n=nums.size();
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
