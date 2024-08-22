eeffee5b5a08c9e0f214126320401e61a00f9f75
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-08-22 10:56:07 +0530

    Time: 250 ms (85.43%) | Memory: 122.2 MB (17.80%) - LeetSync

---
+++ Leetcode/295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+    priority_queue<int> maxHeap; // Max heap for the first half of numbers
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Min heap for the second half of numbers
+
+public:
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+
+        // Balance the heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
+    }
+};
