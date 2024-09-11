2c2b27295a945e34aa3f820c7b4f90b81f94e05b
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-09-11 15:06:14 +0530

    Time: 3 ms (41.47%) | Memory: 7.6 MB (81.24%) - LeetSync

--- Leetcode/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ Leetcode/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,11 @@
 class Solution {
+    int bit(int num,int i){
+        if(num&(1<<(i-1))) return 1;
+        return 0;
+    }
 public:
     int minBitFlips(int start, int goal) {
+        /* Approch 1 using STL: 
         bitset<32>dec1(start);
         bitset<32>dec2(goal);
         int count=0;
@@ -8,5 +13,15 @@ public:
             if(dec2[i]!=dec1[i]) count++;
         }
         return count;
+        */
+        //Approch 2 
+        // int count=0;
+        // for(int i=1;i<32;i++){
+        //     if(bit(start,i) ^ bit(goal,i)) count++; //This can also be if(!(bit(start,i)&bit(goal,i))) count++;
+        // }
+        // return count;
+        
+        // Approch 3 
+        return __builtin_popcount(start^goal);
     }
 };
