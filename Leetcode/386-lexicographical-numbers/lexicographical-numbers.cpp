11910ddf2868dce963481fe54c21a3154fb53f7e
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-09-21 21:07:02 +0530

    Time: 7 ms (75.99%) | Memory: 13.6 MB (82.40%) - LeetSync

--- Leetcode/386-lexicographical-numbers/lexicographical-numbers.cpp
+++ Leetcode/386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -5,11 +5,29 @@ public:
         string d=to_string(b);
         return c<d;
     }
+    void solve(int cur,int &n,vector<int>&ans){
+        if(cur>n) return ;
+        ans.push_back(cur);
+        for(int i=0;i<10;i++){
+            int j=cur*10+i;
+            if(j<=n){
+                solve(j,n,ans);
+            }
+        }
+    }
     vector<int> lexicalOrder(int n) {
         //approch 1 Sorting T.C NLogN
-        vector<int>ans(n);
-        for(int i=0;i<n;i++) ans[i]=i+1;
-        sort(ans.begin(),ans.end(),comp);
+        // vector<int>ans(n);
+        // for(int i=0;i<n;i++) ans[i]=i+1;
+        // sort(ans.begin(),ans.end(),comp);
+        // return ans;
+        
+
+        //approch 2
+        vector<int>ans;
+        for(int i=1;i<=9;i++){
+            solve(i,n,ans);
+        }
         return ans;
     }
 };
