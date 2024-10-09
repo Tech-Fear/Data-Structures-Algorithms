af1665daeb3aaa9104052e8ee38186eec58aafa7
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-10-09 10:28:23 +0530

    Time: 3 ms (42.94%) | Memory: 7.8 MB (81.46%) - LeetSync

--- Leetcode/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ Leetcode/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char>stk;
-        int closingNotMatched=0;
-        for(char &ch:s){
+        // Using stack TC: O(len(s)) and SC: O(N)
+        // stack<char>stk;
+        // int closingNotMatched=0;
+        // for(char &ch:s){
+        //     if(ch=='('){
+        //         stk.push(ch);
+        //     }else{
+        //         if(stk.empty()){
+        //             closingNotMatched++;
+        //         }
+        //         else stk.pop();
+        //     }
+        // }
+        // return stk.size()+closingNotMatched;
+
+        // Approch 2 TC: O(len(s)) and space Complexity: O(1)
+
+        int open=0,closingUnmatch=0;
+        for(auto &ch:s){
             if(ch=='('){
-                stk.push(ch);
+                open++;
             }else{
-                if(stk.empty()){
-                    closingNotMatched++;
+                if(open==0){
+                    closingUnmatch++;
+                }else{
+                    open--;
                 }
-                else stk.pop();
             }
         }
-        return stk.size()+closingNotMatched;
+        return open+closingUnmatch;
     }
 };
