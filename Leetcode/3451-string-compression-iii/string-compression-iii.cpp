deb1dbc32e089557b392f40cfaed6a4a9bc1d768
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-11-04 02:53:19 +0000

    Time: 16 ms (82.05%) | Memory: 29.3 MB (75.40%) - LeetSync

--- Leetcode/3451-string-compression-iii/string-compression-iii.cpp
+++ Leetcode/3451-string-compression-iii/string-compression-iii.cpp
@@ -9,7 +9,7 @@ public:
                 count++;
                 i++;
             }
-            ans.push_back(count+'0');
+            ans.push_back(count+'0'); // used this approch because " ans+=to_string(count) + c;" was causing time complexity to increase due to concatenation. 
             ans.push_back(c);
         }
         return string(ans.begin(),ans.end());
