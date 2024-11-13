616382ed27ad4f8dc1e54aa0de7f4979080e9310
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2024-11-13 04:44:25 +0000

    Time: 0 ms (100.00%) | Memory: 7.5 MB (26.78%) - LeetSync

--- Leetcode/231-power-of-two/power-of-two.cpp
+++ Leetcode/231-power-of-two/power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && __builtin_popcount(n) ==1;
+        // return n>0 && __builtin_popcount(n) ==1;
+        return n>0 && (n&(n-1)) == 0;
     }
 };
