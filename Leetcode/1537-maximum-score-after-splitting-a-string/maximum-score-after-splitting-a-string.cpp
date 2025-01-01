23610b546c669cb23162244c15cfb144026a3934
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-01-01 07:32:31 +0000

    Time: 0 ms (100.00%) | Memory: 8.7 MB (38.26%) - LeetSync

--- Leetcode/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ Leetcode/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int total(string l,string r){
-        int tot=0;
-        for(int i=0;i<r.length();i++){
-            tot+=(r[i]-'0');
-        }
-        for(int i=0;i<l.length();i++){
-            if(l[i]=='0'){
-                tot+=1;
+    int maxScore(string s) {
+        int n = s.length();
+        int leftZeros = 0, rightOnes = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                rightOnes++;
             }
         }
-        return tot;
-    }
-    int maxScore(string s) {
-        int maxi=0;
-        int n=s.length();
-        for(int i=1;i<n;i++){
-            int tot=0;
-            string l=s.substr(0,i);
-            string r=s.substr(i,n);
-            tot=total(l,r);
-            maxi=max(maxi,tot);
+        
+        int maxi = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == '0') {
+                leftZeros++;
+            }
+
+            if (s[i] == '1') {
+                rightOnes--;
+            }
+
+            int tot = leftZeros + rightOnes;
+            maxi = max(maxi, tot);
         }
+
         return maxi;
     }
 };
