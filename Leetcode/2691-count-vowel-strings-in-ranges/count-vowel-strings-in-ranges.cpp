5e6e611013ec6719f4a83d0ea2d6eb1b0f495504
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-01-02 05:46:02 +0000

    Time: 4 ms (81.49%) | Memory: 70.4 MB (48.22%) - LeetSync

---
+++ Leetcode/2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        unordered_set<char>vow={'a','e','i','o','u'};
+        vector<int>isMatch(words.size(),0);
+        int j=0;
+        for(auto &i:words){
+            int n=i.length();
+            if(vow.find(i[0])!=vow.end() && vow.find(i[n-1])!=vow.end()){
+                isMatch[j]=1;
+            }
+            j++;
+        }
+        vector<int>preSum(words.size());
+        preSum[0]=isMatch[0];
+        for(int i=1;i<words.size();i++){
+            preSum[i]=preSum[i-1]+isMatch[i];
+        }
+        vector<int>ans;
+        for(auto &que:queries){
+            int l=que[0];
+            int r=que[1];
+            if(l==0){
+                ans.push_back(preSum[r]);
+            }
+            else{
+                ans.push_back(preSum[r]-preSum[l-1]);
+            }
+        }
+        return ans;
+    }
+};
