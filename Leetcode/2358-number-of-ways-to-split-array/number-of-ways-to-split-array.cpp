68ed949735dbfbfca4b248d44e3eb9e19198a3ee
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-01-03 14:04:51 +0000

    Time: 0 ms (100.00%) | Memory: 89.3 MB (79.17%) - LeetSync

--- Leetcode/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ Leetcode/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -5,15 +5,15 @@ public:
         if (n == 1) {
             return 0;
         }
-        vector<long long> preSum(n, 0);
-        preSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            preSum[i] = preSum[i - 1] + nums[i];
+        long long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
-        long long totalSum = preSum[n - 1];
+        long long leftSum = 0;
         int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (preSum[i - 1] >= totalSum - preSum[i - 1]) {
+        for (int i = 0; i < n - 1; i++) {
+            leftSum += nums[i];
+            if (leftSum >= totalSum - leftSum) {
                 count++;
             }
         }
