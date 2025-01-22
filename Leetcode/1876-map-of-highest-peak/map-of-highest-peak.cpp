e086fcdfd63e3baec39a23781c137507150b6679
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-01-22 04:06:27 +0000

    Time: 72 ms (80.10%) | Memory: 112.7 MB (91.38%) - LeetSync

---
+++ Leetcode/1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> height(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+        vector<int> dirs = {-1, 0, 1, 0, -1};
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dirs[i], ny = y + dirs[i + 1];
+
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
+                    height[nx][ny] == -1) {
+                    height[nx][ny] = height[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        return height;
+    }
+};
