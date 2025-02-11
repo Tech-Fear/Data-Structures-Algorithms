edcffd2521cab77fd6dfd680ede0311e592179ba
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-02-11 01:59:34 +0000

    Time: 14 ms (5.85%) | Memory: 26.2 MB (5.32%) - LeetSync

--- Leetcode/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ Leetcode/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while (s.find(part) != string::npos) {
-            s.erase(s.find(part), part.length());
+    bool checkMatch(stack<char>& stk, string& part, int partLength) {
+        stack<char> temp = stk;
+        for (int partIndex = partLength - 1; partIndex >= 0; partIndex--) {
+            if (temp.top() != part[partIndex]) {
+                return false;
+            }
+            temp.pop();
         }
-        return s;
+        return true;
+    }
+    string removeOccurrences(string s, string part) {
+        stack<char> stk;
+        int strLength = s.length();
+        int partLength = part.length();
+        for (int index = 0; index < strLength; index++) {
+            stk.push(s[index]);
+            if (stk.size() >= partLength && checkMatch(stk, part, partLength)) {
+                for (int j = 0; j < partLength; j++) {
+                    stk.pop();
+                }
+            }
+        }
+        string result = "";
+        while (!stk.empty()) {
+            result = stk.top() + result;
+            stk.pop();
+        }
+        return result;
     }
 };
