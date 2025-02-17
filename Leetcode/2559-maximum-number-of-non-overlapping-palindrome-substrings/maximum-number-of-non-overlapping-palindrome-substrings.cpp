b8055bb5d029f54b88f3162ffa9b4ef0ccf5802a
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-02-17 23:12:42 +0000

    Time: 638 ms (10.29%) | Memory: 128 MB (22.22%) - LeetSync

--- Leetcode/2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ Leetcode/2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -2,33 +2,47 @@ class Solution {
 public:
     int maxPalindromes(string s, int k) {
         int n = s.length();
-        vector<pair<int,int>>palind;
-        auto expand=[&](int left,int right){
-            while(left>=0 && right<n && s[left]==s[right]){
-                if(right-left+1>=k){
-                    palind.push_back({left,right});
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (j == i + 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
                 }
-                left--;
-                right++;
             }
-        };
-        for(int i=0;i<n;i++){
-            expand(i,i);
-            expand(i,i+1);
         }
-        sort(palind.begin(),palind.end(),[&](pair<int,int>&p1,pair<int,int>&p2){
-            return p1.second<p2.second;
+
+        vector<pair<int, int>> palindromes;
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + k - 1; j < n; ++j) {
+                if (dp[i][j]) {
+                    palindromes.push_back({i, j});
+                }
+            }
+        }
+        
+        sort(palindromes.begin(), palindromes.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.second < b.second;
         });
-        int count=0;
-        int lastEnd=-1;
-        for(auto &p:palind){
-            int start=p.first;
-            int end=p.second;
-            if(start>lastEnd){
-                lastEnd=end;
+        
+        int count = 0;
+        int lastEnd = -1;
+        
+        for (const auto& palindrome : palindromes) {
+            int start = palindrome.first;
+            int end = palindrome.second;
+            
+            if (start > lastEnd) {
                 count++;
+                lastEnd = end;
             }
         }
+
         return count;
     }
 };
