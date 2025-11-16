7a7e12e2f3c9b64ff249fe61c7712327bb6092a6
Ajeet Singh <146371492+Tech-Fear@users.noreply.github.com>
2025-11-16 08:21:54 +0530

    Time: 4 ms (19.79%) | Memory: 11.6 MB (39.33%) - LeetSync

---
+++ Leetcode/1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    int numSub(string s) {
+        long long count = 0;
+        long long count1 = 0;
+
+        for (char c : s) {
+            if (c == '1') {
+                count1++;
+            } else {
+                count = (count + (count1 * (count1 + 1) / 2) % mod) % mod;
+                count1 = 0;
+            }
+        }
+
+        if (count1 > 0) {
+            count = (count + (count1 * (count1 + 1) / 2) % mod) % mod;
+        }
+
+        return count;
+    }
+};
